50e33c1a1a6f390509da55c0f11956f5fae000c0
sadhin28 <taosifsadhin113@gmail.com>
2025-05-14 00:54:11 +0600

    add footer

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 
 const Root = () => {
     return (
         <div>
-            <h1>Root App</h1>
+            <Navbar></Navbar>
         </div>
     );
 };
