171ce3c9e574d13bfd6f6187e4ccb5a13cf226a8
sadhin28 <taosifsadhin113@gmail.com>
2025-05-14 01:43:28 +0600

    add book list component

---
+++ src/Components/Booklist/Booklist.jsx
@@ -0,0 +1,9 @@
+const Booklist = () => {
+    return (
+        <div>
+            <h1>Book List</h1>
+        </div>
+    );
+};
+
+export default Booklist;
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Root from './Components/Root/Root.jsx';
 import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
 import Home from './Components/Home/Home.jsx';
 import Dashboard from './Components/Dashboard/Dashboard.jsx';
+import Booklist from './Components/Booklist/Booklist.jsx';
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
         element:<Dashboard></Dashboard>
         
       },
+      {
+        path:"booklist",
+        element:<Booklist></Booklist>
+      }
      
     ]
 
