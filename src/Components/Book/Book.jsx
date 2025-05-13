5e0cb0f3b704df7bcba5eb3a4b30eadcc435ed91
sadhin28 <taosifsadhin113@gmail.com>
2025-05-14 05:00:26 +0600

    add book card

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -1,7 +1,7 @@
 const Book = ({ data }) => {
     const { bookName, author, image, } = data;
     return (
-        <div className=" border border-gray-300 card w-[375px] bg-[1px	Inside
+        <div className=" border border-gray-300 card md:w-[385px] w-[400px] bg-[1px	Inside
             #13131326] w-76 shadow-sm">
             <figure className=" border border-gray-500 rounded-xl m-5 bg-gray-300 px-5 pt-5 pb-5">
                 <img
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     </>
     return (
 
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="sticky top-0 z-30 navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
