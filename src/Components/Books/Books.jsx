b173fb189218a0558d5403162e75e0899675d88b
sadhin28 <taosifsadhin113@gmail.com>
2025-05-14 03:45:31 +0600

    add book component

---
+++ src/Components/Books/Books.jsx
@@ -0,0 +1,10 @@
+
+const Books = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-black text-center">Books</h1>
+        </div>
+    );
+};
+
+export default Books;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,17 @@
 import Banner from "../Banner/Banner";
+import Books from "../Books/Books";
 
 const Home = () => {
     return (
         <div className="">
-           <div className="mt-10 mb-20">
+           <div className="mt-10 mb-10">
                 <Banner></Banner>
+           </div>
+           <div className="mb-10">
+             <Books></Books>
+                <div>
+                    
+                </div>
            </div>
         </div>
     );
