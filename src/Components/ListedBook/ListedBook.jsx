bbaafc4c3f3fd12b5f6dac74166f5bfa7a31d48f
sadhin28 <taosifsadhin113@gmail.com>
2025-05-15 01:12:53 +0600

    add listed book

--- src/Components/ListedBook/ListedBook.jsx
+++ src/Components/ListedBook/ListedBook.jsx
@@ -1,12 +1,12 @@
 import { MdContactPage } from "react-icons/md";
 
 const ListedBook = ({data}) => {
-    const {image,bookName,author,tags,totalPages}=data
+    const {image,bookName,category,author,rating,tags,totalPages}=data
     return (
-        <div className='border-2 bg-[1px card	Inside
+        <div className='border border-gray-300 bg-[1px card	Inside
 #13131326]'>
-            <div className='flex md:gap-20 gap-5 p-5'>
-                <img src={image} className='w-20' alt="" />
+            <div className='flex md:gap-20 gap-5 p-5 items-center'>
+                <img src={image} className='w-25 h-40' alt="" />
                <div>
                   <p>{bookName}</p>
                 <p>{author}</p>
@@ -24,8 +24,15 @@ const ListedBook = ({data}) => {
                 </div>
                 
                 <div className='divider'></div>
+                <div className="grid grid-cols-2 gap-5 md:flex ">
+                     <button  className="bg-[#328EFF26] rounded-[30px] text-[16px] text-[#328EFF] w-[125px] h-[33px]">Category : {category}</button>
+                     <button  className="bg-[#FFAC3326] rounded-[30px] text-[16px] text-[#FFAC33] w-[125px] h-[33px]">Rating: {rating}</button>
+                     <button  className="bg-[#23BE0A] rounded-[30px] text-[16px] text-white w-[125px] h-[33px]">View Details</button>
+                </div>
                </div>
+               
             </div>
+           
         </div>
     );
 };
