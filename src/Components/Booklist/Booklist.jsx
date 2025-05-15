cfcbb4c62b8f041fdb953ce141ed7792676b6a5b
sadhin28 <taosifsadhin113@gmail.com>
2025-05-15 23:12:28 +0600

    sort

--- src/Components/Booklist/Booklist.jsx
+++ src/Components/Booklist/Booklist.jsx
@@ -8,59 +8,85 @@ import ListedBook from '../ListedBook/ListedBook';
 import WishList from '../WishList/WishList';
 
 const Booklist = () => {
-    const deletelocalStore=()=>{
+    const deletelocalStore = () => {
         localStorage.clear()
     }
     const [readlist, setredlist] = useState([])
     const allBooks = useLoaderData()
-    const [wishes,setwishes]=useState([])
-    
+    const [wishes, setwishes] = useState([])
+    const [sort,setsort]=useState('')
+  
+
     useEffect(() => {
         const storeReadlist = getStoredReadList();
         const storedListint = storeReadlist.map(id => parseInt(id));
         const booklist = allBooks.filter(book => storedListint.includes(book.bookId))
         setredlist(booklist)
-          
-        
+
+
     }, [])
-    useEffect(()=>{
+    useEffect(() => {
         const storewishes = getStoredwhishesList();
-         
-         const storewishesInt = storewishes.map(id=> parseInt(id));
-         console.log(storewishesInt)
-           const wishbook = allBooks.filter(book => storewishesInt.includes(book.bookId))
-           setwishes(wishbook)
-         
-    },[])
-    
+
+        const storewishesInt = storewishes.map(id => parseInt(id));
+        console.log(storewishesInt)
+        const wishbook = allBooks.filter(book => storewishesInt.includes(book.bookId))
+        setwishes(wishbook)
+
+    }, [])
+
+
+    const handelSortbyrating =sortType=>{
+        setsort(sortType);
+        if(sortType === 'rating'){
+            const sortbyRating= [...readlist].sort((a,b)=>a.rating - b.rating);
+            setredlist(sortbyRating);
+        }
+    }
+    const handelSortbypages =sortType=>{
+        setsort(sortType)
+        if(sortType === 'Pages'){
+            const sortedreadlist =[...readlist].sort((a,b)=>a.totalPages - b.totalPages)
+            setredlist(sortedreadlist);
+        }
+    }
     return (
         <div className='mb-10'>
-           
+            
+
             <Tabs className="mt-5">
                 <TabList className='font-bold'>
                     <Tab>Wishlist Books</Tab>
                     <Tab>Wishlist Books</Tab>
                 </TabList>
 
-                <TabPanel className='mt-10'>
-                   
+                <TabPanel className='mt-10 '>
+                    <div className='mb-5 text-center'>
+                <details className="dropdown ">
+                    <summary className="btn text-xl  text-white  rounded-2xl bg-[rgb(3,170,0)] hover:bg-[rgb(35,190,10)] m-1">{sort?`Sort by ${sort}`: "Sort By"}</summary>
+                    <ul className="menu duration-1000 dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li className='hover:duration-300 hover:bg-black hover:rounded-2xl hover:text-white' onClick={()=>handelSortbyrating('rating')}><a className='rounded-2xl'>Ratings</a></li>
+                        <li className='hover:duration-300 hover:bg-black hover:rounded-2xl hover:text-white' onClick={()=>handelSortbypages('Pages')}><a className='rounded-2xl'>No Of Pages</a></li>
+                    </ul>
+                </details>
+            </div>
                     <div className='px-2 grid gap-5'>
-                         
+
                         {
-                            
-                            readlist.map(data =><ListedBook  data={data} key={data.bookId}></ListedBook>)
+
+                            readlist.map(data => <ListedBook data={data} key={data.bookId}></ListedBook>)
                         }
                     </div>
                 </TabPanel>
                 <TabPanel className="mt-10">
-                      <div className='px-2 grid gap-5'>
-                          {
-                            wishes.map(data=><WishList data={data} key={data.bookId}></WishList>)
-                          }
-                      </div>
+                    <div className='px-2 grid gap-5'>
+                        {
+                            wishes.map(data => <WishList data={data} key={data.bookId}></WishList>)
+                        }
+                    </div>
                 </TabPanel>
             </Tabs>
-            <button onClick={deletelocalStore} className='btn mt-5 hover:bg-[#23BE0A] hover:text-white'>Clean Read Book</button>
+            <button onClick={deletelocalStore} className='btn mt-5 hover:bg-[rgb(35,190,10)] hover:text-white'>Clean Read Book</button>
         </div>
     );
 };
